3e03413f83da0b8883309144033fdef774ce31db
taher <tasd871@gmail.com>
2021-11-27 00:33:27 +0330

    created first app
    the first app name is website with tree views such as home,about,contacts

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from website.views import index_view,about_view,contact_view
+
+urlpatterns = [
+    path('',index_view),
+    path('about',about_view),
+    path('contacts',contact_view)
+]
---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse,JsonResponse
+
+def index_view(request):
+    return HttpResponse('<h1>Home Page <h1/>')
+
+def about_view(request):
+    return HttpResponse('<h1>about Page <h1/>')
+
+def contact_view(request):
+    return HttpResponse('<h1>contacts Page <h1/>')
+  
