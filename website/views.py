e4148270ceb7d44cc5024d505fbacb72afc2ea72
taher <tasd871@gmail.com>
2021-11-27 20:26:34 +0330

    created temlate html file
    added temlates folder and html files to website app and render to views

--- website/views.py
+++ website/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page <h1/>')
+    return render(request,'index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>about Page <h1/>')
+    return render(request,'about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>contacts Page <h1/>')
+    return render(request,'contact.html')
   
