94f375567ab5f27dfdb5076a4ee0cdf1341a284f
taher <tasd871@gmail.com>
2021-12-04 23:56:35 +0330

    add connect to admin
    added connect to admin and add some feature to it

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from website.models import Connect
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'create_date'
+    list_display = ('name','email','create_date')
+    list_filter = ('email',)
+    search_fields = ('name','message')
+
+admin.site.register(Connect,ContactAdmin)
--- website/models.py
+++ website/models.py
@@ -8,3 +8,6 @@ class Connect(models.Model):
     massage = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
