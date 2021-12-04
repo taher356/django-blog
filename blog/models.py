01d78f23dec881aaf70f65184028a88e30f38c10
taher <tasd871@gmail.com>
2021-12-04 15:02:34 +0330

    create blog and website models

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class PostModel(models.Model):
+    #image
+    # author
     title = models.CharField(max_length=255)
     content = models.TextField()
+    #tags
+    #category
+    counted_view = models.IntegerField(default=0)
+    status = models.IntegerField(default=False)
+    publish_date = models.DateTimeField(null =True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now =True)
