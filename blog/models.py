09ddcd372bfea4673507dc2e3c2bad7496de1416
taher <tasd871@gmail.com>
2021-12-04 17:41:54 +0330

    django admin panel
    added blog app to the admin page

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,6 @@ class PostModel(models.Model):
     publish_date = models.DateTimeField(null =True)
     create_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now =True)
+
+    def __str__(self):
+        return "{}-{}".format(self.title,self.id)
