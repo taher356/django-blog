683967de3f18ebf7b169e8fa37dea178996526a0
taher <tasd871@gmail.com>
2021-12-04 21:34:23 +0330

    admin panel costomisition
    added some panel to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import PostModel
 
-admin.site.register(PostModel)
-
+class PostModelAdmin(admin.ModelAdmin):
+    date_hierarchy = 'create_date'
+    empty_value_display = '-empty-'
+    list_display = ('title','counted_view','status','publish_date')
+    list_filter = ('status',)
+    search_fields = ['title','content']
+admin.site.register(PostModel,PostModelAdmin)
