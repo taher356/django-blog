0d2ec8a17b3687538cc55e026330893e36833edc
taher <tasd871@gmail.com>
2021-12-06 23:12:24 +0330

    test for query to database

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from blog.models import PostModel
 # Create your views here.
 def blog_view(request):
-    Post = PostModel.objects.filter(status=1)
+    Post = PostModel.objects.filter(status=1 )
     context = {'PostModel':Post}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request):
     return render(request,'blog/blog-single.html')
 
-def test(request):
-    Post = PostModel.objects.all()
-    context = {'PostModel':Post}
+def test(request,pid):
+    post = PostModel.objects.get(id=pid)
+    context = {'post':post}
     return render(request,'test.html',context)    
