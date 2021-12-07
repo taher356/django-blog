d888647ef25712414e6ece88929be67688d549d0
taher <tasd871@gmail.com>
2021-12-08 00:53:19 +0330

    query and template for show what we have done

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import PostModel
 # Create your views here.
 def blog_view(request):
-    Post = PostModel.objects.filter(status=1 )
-    context = {'PostModel':Post}
+    posts = PostModel.objects.filter(status=1)
+    context = {'PostModel':posts}
     return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+    posts = PostModel.objects.filter(status=1)
+    post = get_object_or_404(posts,pk=pid)
+    context = {'post':post}
+    return render(request,'blog/blog-single.html',context)
 
 def test(request,pid):
     post = PostModel.objects.get(id=pid)
