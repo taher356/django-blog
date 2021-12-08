75bc09f76df1daf22a5a30d2a8cd20ffa4873d7c
taher <tasd871@gmail.com>
2021-12-08 23:50:23 +0330

    added author

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from blog.models import PostModel
 class PostModelAdmin(admin.ModelAdmin):
     date_hierarchy = 'create_date'
     empty_value_display = '-empty-'
-    list_display = ('title','counted_view','status','publish_date')
-    list_filter = ('status',)
+    list_display = ('title','author','counted_view','status','publish_date')
+    list_filter = ('status','author')
     search_fields = ['title','content']
     #ordering = ['create_date']
 admin.site.register(PostModel,PostModelAdmin)
