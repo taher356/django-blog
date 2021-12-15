3bbd68ec35df7dab182549f2854dc7b06eec862a
taher <tasd871@gmail.com>
2021-12-15 22:49:13 +0330

    create author directory in blog view

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',blog_view,name='index'),
     path('<int:pid>',blog_single,name='single'),
     path('test',test,name='test'),
-    path('category/<str:cat_name>',blog_view,name='category')
+    path('category/<str:cat_name>',blog_view,name='category'),
+    path('author/<str:author_username>',blog_view,name='author')
 ]
