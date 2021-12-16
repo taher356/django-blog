74d302d406aae7f3e2a06a7277e963c4c68249bf
taher <tasd871@gmail.com>
2021-12-16 23:22:48 +0330

    paginator in view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import PostModel
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def blog_view(request,**kwargs):
    
@@ -8,8 +9,19 @@ def blog_view(request,**kwargs):
         posts = PostModel.objects.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username')!=None :
         posts = PostModel.objects.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+
+    except  EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'PostModel':posts}
     return render(request,'blog/blog-home.html',context)
+
 
 def blog_single(request,pid):
     posts = PostModel.objects.filter(status=1)
